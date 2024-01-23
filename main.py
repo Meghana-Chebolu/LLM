d2d1158d77b5bdba66760710d4625a7699f29b49
Monika kusumanchi <kusumanchimonika1407@gmail.com>
2024-01-23 06:59:53 +0000

    llm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query,Form
 from pydantic import BaseModel
 import os
 import openai
@@ -6,43 +6,43 @@ import google.generativeai as genai
 
 app = FastAPI()
 
-# Retrieve the OpenAI API key from the command line or use an empty string if not provided
-openai.api_key = os.getenv("OPENAI_API_KEY", default="")
-class ChatGPTRequest(BaseModel):
-    model: str = "gpt-3.5-turbo"
-    prompt: str = "print hello world!"
-    temperature: float = 0
-    max_tokens: int = 256
-    top_p: float = 1
-    frequency_penalty: float = 0.0
-    presence_penalty: float = 0.0
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.post("/bot_query")
+def bot_query(input_text: str):
+    return {"output_text": input_text.lower()}
+
+openai.api_key = os.getenv("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", default="")
+
 @app.post("/get_chatGPT_completion")
-def get_chatGPT_completion(chat_request: ChatGPTRequest):
+def get_chatGPT_completion(
+    api_key: str =Form(...),
+    prompt: str = Form(...),  
+):
     try:
-        messages = [{"role": "user", "content": chat_request.prompt}]
+        openai.api_key = api_key  # Set the API key
+        model: str = "gpt-3.5-turbo"
+        temperature: float = 0
+        max_tokens: int = 256
+        top_p: float = 1
+        frequency_penalty: float = 0.0
+        presence_penalty: float = 0.0
+
+        messages = [{"role": "user", "content": prompt}]
 
         response = openai.ChatCompletion.create(
-            model=chat_request.model,
+            model=model,
             messages=messages,
-            temperature=chat_request.temperature,
-            max_tokens=chat_request.max_tokens,
-            top_p=chat_request.top_p,
-            presence_penalty=chat_request.presence_penalty,
-            frequency_penalty=chat_request.frequency_penalty,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            top_p=top_p,
+            presence_penalty=presence_penalty,
+            frequency_penalty=frequency_penalty,
         )
 
         resp = response['choices'][0]['message']['content']
         return {"response": resp}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-    return response
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    # Get the OpenAI API key from the command line
-    openai.api_key = input("Enter your OpenAI API Key: ")
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
