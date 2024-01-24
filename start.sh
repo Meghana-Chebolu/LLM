f3cb302374c403b863cb533f332929e208ab96b3
Monika kusumanchi <kusumanchimonika1407@gmail.com>
2024-01-24 04:51:38 +0000

    fronend changes

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
- docker build -t react-frontend .
+ docker build -t fastapi-backend
+  .
  docker run -p 8000:8000 fastapi-backend
 
  docker build -t react-frontend .
  docker run -p 3000:3000 react-frontend
 
- npm start
+ cd frontend
+ npm install
  docker compose build 
  docker compose up
